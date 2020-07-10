7fb20b4bebd57e63b703c2b9738dd381e92cc6fd
travisprol0 <travisjohnprol@gmail.com>
2020-07-10 12:00:09 -0400

    done

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,6 +2,12 @@ class VendorsController < ApplicationController
 
   def index
     @vendors = Vendor.all
+    @vendor_sweets = VendorSweet.all
+  end
+
+  def show
+    @vendor = Vendor.find(params[:id])
+    @vendor_sweets = VendorSweet.all
   end
 
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,10 @@
 class Vendor < ApplicationRecord
+    has_many :vendor_sweets
+    has_many :sweets, through: :vendor_sweets
 
+
+    def average
+        @vendor = Vendor.find(params[:id])
+        @vendor.sweets
+    end
 end
